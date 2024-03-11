a23aef9e7152e016d41222265e6771311c91df45
PachaiyappanS <161188700+PachaiyappanS@users.noreply.github.com>
2024-03-11 19:26:23 +0530

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,24 @@
+function ValidateForm(event){
+    event.preventDefault()
+    var name = document.getElementById('name').value;
+    var Age = document.getElementById('Age').value;
+    var email = document.getElementById('email').value;
+    var pass = document.getElementById('pass').value;
+    var letters = /^[0-9]+$/;
+
+    if(name.trim()=="" || letters.test(name)){
+        alert("Please enter Your Name");
+        return false;
+    }
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(email)){
+        alert("Please enter a Valid Email Address");
+        return false;
+    }
+
+    if(pass.length<6){
+        alert("Password must be at least 6 Characters long");
+        return false;
+    }
+    alert("Form Submitted Successfully");
+}
